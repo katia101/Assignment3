271446df106b640458f985b2f91c4dda30d24d48
katia101 <217149073@mycput.ac.za>
2021-06-11 23:24:52 +0200

    5th commit

--- src/main/java/za/ac/cput/Factory/BookingFactory.java
+++ src/main/java/za/ac/cput/Factory/BookingFactory.java
@@ -1,4 +1,21 @@
 package za.ac.cput.Factory;
 
-public class BookingFactory {
+import za.ac.cput.Entity.Booking;
+
+public class BookingFactory 
+{
+ public static Booking createBooking(int bookID, String bookTime, String bookDate, String moduleCode, String moduleName)
+ {
+     Booking booking = new Booking.Builder()
+             .setBookID(bookID)
+             .setBookTime(bookTime)
+             .setBookDate(bookDate)
+             .setModuleCode(moduleCode)
+             .setModuleName(moduleName)
+             .build();
+
+     return booking;
+
+
+ }
 }
--- src/main/java/za/ac/cput/Factory/UserBookingFactory.java
+++ src/main/java/za/ac/cput/Factory/UserBookingFactory.java
@@ -1,4 +1,17 @@
 package za.ac.cput.Factory;
 
-public class UserBookingFactory {
+import za.ac.cput.Entity.UserBooking;
+
+public class UserBookingFactory 
+{
+    public static UserBooking createUserBooking(int userID, int bookID)
+    {
+        UserBooking userBooking = new UserBooking.Builder()
+                .setUserID(userID)
+                .setBookID(bookID)
+                .build();
+
+        return userBooking;
+
+    }
 }
